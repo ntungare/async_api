1964693e74ba96ec4d9d8e441591d84105c7db97
Ninad T <ninad.tungare@gmail.com>
2020-12-23 11:27:11 -0600

    updating to latest

--- async_api/app.py
+++ async_api/app.py
@@ -6,7 +6,12 @@ from async_api.routes import files, graphql, items, models, root, user
 
 origins = ["http://localhost", "http://localhost:8080", "*"]
 
-app = FastAPI()
+app = FastAPI(
+    openapi_url="/private/openapi.json",
+    docs_url="/private/docs",
+    redoc_url="/private/redoc",
+    swagger_ui_oauth2_redirect_url="/private/docs/oauth2-redirect",
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -17,9 +22,9 @@ app.add_middleware(
 app.add_middleware(middleware_class=middleware_1.Middleware1)
 app.add_middleware(middleware_class=middleware_2.Middleware2)
 app.add_middleware(middleware_class=middleware_3.Middleware3)
-app.include_router(router=root.root_router, tags=["root_routes"])
-app.include_router(router=items.items_router, tags=["items_routes"], prefix="/items")
-app.include_router(router=user.users_router, tags=["users_routes"], prefix="/users")
-app.include_router(router=models.models_router, tags=["models_routes"], prefix="/model")
-app.include_router(router=files.files_router, tags=["files_routes"], prefix="/files")
-app.include_router(router=graphql.graphql_router, tags=["graphql_routes"], prefix="/graphql")
+app.include_router(router=root.root_router, tags=["routes", "root_routes"])
+app.include_router(router=items.items_router, tags=["routes", "items_routes"], prefix="/items")
+app.include_router(router=user.users_router, tags=["routes", "users_routes"], prefix="/users")
+app.include_router(router=models.models_router, tags=["routes", "models_routes"], prefix="/model")
+app.include_router(router=files.files_router, tags=["routes", "files_routes"], prefix="/files")
+app.include_router(router=graphql.graphql_router, tags=["routes", "graphql_routes"], prefix="/graphql")
